fcc19b13ca885518fb2a85a99d233b4fc616f0ff
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2022-01-20 12:56:53 +0100

    passaggio a .NET 6

--- frontend/Models/DettaglioEvento.cs
+++ frontend/Models/DettaglioEvento.cs
@@ -1,12 +1,19 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace frontend.Models
 {
     public class DettaglioEvento
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Il nome è obbligatorio!")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "La località è obbligatoria!")]
         public string Localita { get; set; }
+
+        [Required(ErrorMessage = "La data è obbligatorio!")]
         public DateTime Data { get; set; }
         public string Descrizione { get; set; }
         public string Note { get; set; }
